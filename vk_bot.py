87ad28cec80e91c27bc884a84f045e1d1fb9317c
Dmitrii Sverlov <dmitry@sverlov.com>
2023-07-07 21:14:45 +0300

    Second commit

--- vk_bot.py
+++ vk_bot.py
@@ -28,7 +28,10 @@ class BotInterface():
                                  host=os.getenv('host'), 
                                  port=os.getenv('password')) 
         self.offset = 0     
-     
+        self.users = []
+        self.params = {}
+
+
     def write_message(self, user_id, message, keyboard=None, attachment=None):
         try:
             keyboard_data = keyboard.get_keyboard()
@@ -40,13 +43,41 @@ class BotInterface():
                                 'random_id': get_random_id(),
                                 'keyboard': keyboard_data, 
                                 'attachment': attachment})
-        
-    def start(self):
-        
+    
+    def check_exit(self):
         
         for event in self.longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                answer = event.text.lower()
+                return answer
+             
+     
+    def check_users(self, sender, users):
+         
+        user = users.pop()
+        vk_id = user['id']
+        vk_url = f'https://vk.com/{user["id"]}'
+                         
+        if self.database.check_usersvk(vk_id):return
+        photos = self.handler.get_top_photos(user)
             
-            self.database.create_table()
+        message = (f'Встречайте, это - {user["name"]}!\nВот ссылка на страницу: {vk_url}\n'
+                   f'Вот лучшие фото со страницы:')
+        self.write_message(sender, message, self.keyboard)
+                
+        attachment = []
+        for photo in photos:
+            attachment.append(f"photo{photo['owner_id']}_{photo['id']}")
+        self.write_message(sender, '', self.keyboard, attachment=','.join(attachment))
+        self.database.save_usersvk(vk_id, vk_url)
+        self.write_message(sender, f'пользователь {vk_url} сохранен')
+
+
+    def start(self):
+        
+        self.database.create_table()
+        for event in self.longpoll.listen():
+            
             
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 command = event.text.lower()
@@ -55,56 +86,49 @@ class BotInterface():
                 if command == 'привет':
                     
                     self.params = self.handler.get_profile_info(sender)
-                     
-                    if self.params["city"] is None:
-                        self.write_message(sender, f'привет, {self.params["first_name"]} {self.params["last_name"]}\n'
-                                           f'для продолжения укажите Ваш город')
-                        self.params["city"] = command
+                        
                     if self.params["sex"] is None:
-                        self.write_message(sender, f'привет, {self.params["first_name"]} {self.params["last_name"]}\n'
+                        self.write_message(sender, f'привет, {self.params["name"]}\n'
                                            f'для продолжения уточните Ваш пол.\n'
                                            f'введите: "муж" или "жен"')
+                        command = self.check_exit()
                         if command == 'муж': 
-                            self.params["sex"] = 1
-                        else:
                             self.params["sex"] = 2
+                        else:
+                            self.params["sex"] = 1
+                    
                     if self.params["bdate"] is None:
-                        self.write_message(sender, f'привет, {self.params["first_name"]} {self.params["last_name"]}\n'
+                        self.write_message(sender, f'привет, {self.params["name"]}\n'
                                            f'для продолжения укажите дату Вашего рождения в формате DD.MM.YYYY')
+                        command = self.check_exit()
                         self.params["bdate"] = command
                     else:
-                        self.write_message(sender, f'привет, {self.params["first_name"]} {self.params["last_name"]}')
+                        self.write_message(sender, f'привет, {self.params["name"]}')
                                             
                     self.write_message(sender,f'я - бот знакомств в ВК.\n'
                                           f'стартуем!?\n мне нужна команда "поиск", увидешь первый результат', self.keyboard)        
  
                 elif command in ('поиск'):
-                    
-                    users = self.handler.search_users(sender,self.offset)
-                    self.offset += 10
-                    for user in users['items']:
-                        vk_id = user['id']
-                        vk_url = f'https://vk.com/{user["screen_name"]}'
-                        if user['is_closed']: continue
-                        if self.database.check_usersvk(vk_id):continue
-                         
-                        photos = self.handler.get_top_photos(user)
-            
-                        message = (f'Встречайте, это - {user["first_name"]} {user["last_name"]}!\nВот ссылка на страницу: {vk_url}\n'
-                                   f'Вот лучшие фото со страницы:')
-                        self.write_message(sender, message, self.keyboard)
-                
-                        attachment = []
-                        for photo in photos:
-                            attachment.append(f"photo{photo['owner_id']}_{photo['id']}")
-                        self.write_message(sender, '', self.keyboard, attachment=','.join(attachment))
-                        self.database.save_usersvk(vk_id, vk_url)
-                        self.write_message(sender, f'пользователь {vk_url} сохранен')
+                    if not self.params:
+                        self.write_message(sender, f'сначала поздоровайся - напиши "привет"')
                         
+                    elif self.users:
+
+                        self.check_users(sender, self.users)
+                         
+                    
+                    else:
+                        self.users = self.handler.search_users(self.params, self.offset)
+                         
+                        self.check_users(sender, self.users)
+                        
+                        self.offset += 10
+
                 elif command in ('пока'):
                     self.write_message(sender, 'жаль прощаться, результаты поиска будут очищены , до новых встреч!')
                     self.database.delete_table() 
                     break
+
                 else:
                     self.write_message(sender, 'команда не опознана')                       
                           
