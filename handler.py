66737b27c5161be1122bc4efa953242bcd639294
Dmitrii Sverlov <dmitry@sverlov.com>
2023-07-07 21:15:21 +0300

    Second commit

--- handler.py
+++ handler.py
@@ -13,47 +13,57 @@ class VkTools():
        self.handler = vk_api.VkApi(token=self.token)
 
     def get_profile_info(self, user_id):
-        info = self.handler.method('users.get', {'user_ids': user_id, 'fields': 'first_name,last_name,sex,city,bdate'})
-        user_info = {
-                        'id': info[0]['id'],
-                        'first_name': info[0]['first_name'],
-                        'last_name': info[0]['last_name'],
-                        'sex': info[0]['sex'],
-                        'city': info[0]['city'],
-                        'bdate': info[0]['bdate'],
-                    }
-        
+
+        info, = self.handler.method('users.get',
+                            {'user_id': user_id,
+                            'fields': 'city,bdate,sex,relation,home_town' 
+                            }
+                            )
+        user_info = {'name': info['first_name'] + ' '+ info['last_name'],
+                     'id':  info['id'],
+                     'bdate': info['bdate'] if 'bdate' in info else None,
+                     'home_town': info['home_town'],
+                     'sex': info['sex'] if 'sex' in info else None,
+                     'city': info['city']['id']
+                     }
         return user_info
-      
-    def search_users(self, user_id, offset):
-        search_params = {'user_ids': user_id, 'fields': 'bdate, sex, city'}
-        user_info = self.handler.method('users.get', search_params)
-        user_info = user_info[0]
-        user_age = self.calculate_age(user_info['bdate'])
-        user_sex = user_info['sex']
-        user_city = user_info['city']['id']
+    
+    def search_users(self, params, offset):
 
-        users = self.handler.method(
-                'users.search',
-                {'count': 10,
-                'city': user_city,
-                'sex': 1 if user_sex == 2 else 2,
-                'status': 0,
-                'status_list': [1, 6],
-                'age_from': user_age - 3,
-                'age_to': user_age + 3,
-                'has_photo': 1,
-                'fields': 'photo_max_orig, screen_name',
-                'offset': offset})
+        sex = 1 if params['sex'] == 2 else 2
+        city = params['city']
+        curent_year = datetime.now().year
+        user_year = int(params['bdate'].split('.')[2])
+        age = curent_year - user_year
+        age_from = age - 3
+        age_to = age + 3
 
-        return users
-         
-    def calculate_age(self, bdate):
-        if bdate:
-            curent_year = datetime.now().year
-            user_year = int(bdate.split('.')[2])
-            age = curent_year - user_year
-            return age
+        users = self.handler.method('users.search',
+                                {'count': 10,
+                                 'offset': offset,
+                                 'age_from': age_from,
+                                 'age_to': age_to,
+                                 'sex': sex,
+                                 'city': city,
+                                 'status': 6,
+                                 'is_closed': False
+                                }
+                            )
+        try:
+            users = users['items']
+        except KeyError:
+            return []
+        
+        res = []
+
+        for user in users:
+            if user['is_closed'] == False:
+                res.append({'id' : user['id'],
+                            'name': user['first_name'] + ' ' + user['last_name']
+                           }
+                           )
+        
+        return res
      
     def get_top_photos(self, user):
         photos_user = self.handler.method('photos.get', {'owner_id': user['id'], 'album_id': 'profile', 'rev': 1,
